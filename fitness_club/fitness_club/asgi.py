eb466e92de78781bdae9f888749da4b73c0983f6
Lei Wang <206961662+leiwangamy@users.noreply.github.com>
2025-12-31 02:35:22 -0800

    Fixed WSGI path

--- fitness_club/fitness_club/asgi.py
+++ fitness_club/fitness_club/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_club.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_club.fitness_club.settings')
 
 application = get_asgi_application()
--- fitness_club/fitness_club/wsgi.py
+++ fitness_club/fitness_club/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_club.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_club.fitness_club.settings')
 
 application = get_wsgi_application()
